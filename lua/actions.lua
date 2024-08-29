05f011fe56cdd9699c8cc357a9d9eb0b5873c61b
Jimmy Tran <jimmytrandev@gmail.com>
2024-08-29 18:27:45 +0000

    feat: ✨ add lookup selected command

--- lua/actions.lua
+++ lua/actions.lua
@@ -15,6 +15,22 @@ local function lookup(search_engine_url, open_command)
   vim.fn.jobstart({ open_command, url })
 end
 
+local function lookupSelected(search_engine_url, open_command)
+  if open_command == nil then
+    open_command = utils.getOpenCommand()
+  end
+
+  local search_query = vim.fn.getreg("*")
+  if search_query == "" then
+    return
+  end
+
+  local encoded_search_query = utils.urlencode(search_query)
+  local url = search_engine_url .. encoded_search_query
+  vim.fn.jobstart({ open_command, url })
+end
+
 return {
-  lookup = lookup
+  lookup = lookup,
+  lookupSelected = lookupSelected,
 }
